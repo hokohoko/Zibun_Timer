c55c06eb429ea9e86a0ad260c93a7990e50a231c
hokohoko <tadak.0v0@gmail.com>
2024-03-17 17:47:52 +0900

    main.dartにfirebaseの初期化処理を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  //おまじない?
+  WidgetsFlutterBinding.ensureInitialized();
+  //firebaseの初期化処理
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
