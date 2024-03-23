6bd8f92c907b021bcb93ca29cdd73e623a4e4587
taka0707 <yukken777@yahoo.ne.jp>
2024-03-23 11:47:05 +0900

    titleを変更

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'タスク選択アプリ',
+      title: 'ZIBUN_TIMER',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
